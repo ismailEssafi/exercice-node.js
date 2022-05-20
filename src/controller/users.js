e98f3a33865afe3e202f5fb6662c5dbaefca85f0
ismail <issmail.essafi@gmail.com>
2022-05-20 14:34:03 +0000

    hash password

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,16 +1,22 @@
 const User = require('../models/user.js');
-
+const jwt = require('jsonwebtoken');
 // exports.signIn = '';
 exports.signUp = async (req, res) => {
     try{
         const user = await User.create(req.body);
+        const id = user._id
+        const token = jwt.sign({id}, process.env.SECRET, {
+            expiresIn : process.env.EXPIRE_IN
+        });
         res.status(200).json({
             status: 'succes',
-            data : user
-        })
-    }catch{
+            data : user,
+            token : token
+        });
+    }catch (error){
         res.status(400).json({
-            status: 'fail'
+            status: 'fail',
+            error : error.message
         })
     }
 }
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const schema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -6,5 +7,12 @@ const schema = new mongoose.Schema({
     password: { type: String, required: true },
     deletedAt: { type: Date, default: null },
 }, { timestamps: true });
+
+schema.pre("save", async function(next){
+    if(!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 module.exports = mongoose.model('user', schema);
