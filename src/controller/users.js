a535271793bf126f4ff1661ae2620be4ed7a755b
ismail <issmail.essafi@gmail.com>
2022-05-20 20:34:11 +0000

    develop getAll, getOne, update,delete user

--- src/controller/users.js
+++ src/controller/users.js
@@ -18,3 +18,54 @@ exports.create = async (req, res) => {
         res.status(500).json({error : error.message})
     }
 }
+
+exports.getAll = async (req, res) => {
+    try{
+        const users = await User.find({});
+        res.status(200).json({
+            status: 'succes',
+            data : users,
+        });
+    }catch (error){
+        res.status(500).json({error : error.message})
+    }
+}
+
+exports.get = async (req, res) => {
+    try{
+        const users = await User.find({_id: req.params.id});
+        res.status(200).json({
+            status: 'succes',
+            data : users,
+        });
+    }catch (error){
+        res.status(500).json({error : error.message})
+    }
+}
+
+exports.update = async (req, res) => {
+    try{
+        const user = await User.findByIdAndUpdate({_id :req.params.id}, req.body);
+        
+        res.status(200).json({
+            status: 'succes',
+            data : user,
+        });
+    }catch (error){
+        res.status(500).json({error : error.message})
+    }
+}
+
+exports.delete = async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete({_id :req.params.id});
+        
+        res.status(200).json({
+            status: 'succes',
+            data : user,
+        });
+    }catch (error){
+        res.status(500).json({error : error.message})
+    }
+}
+
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,6 @@ const app = express.Router();
 const users = require('../controller/users');
 
 app.route('/').post(users.create).get(users.getAll);
-//app.route('/:id').patch(users.update).delete(users.delete).get(users.get)
+app.route('/:id').patch(users.update).delete(users.delete).get(users.get)
 
 module.exports = app;
